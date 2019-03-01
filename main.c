a21c14998fb53c10bad7f4bd905b5dbb2b27b80d
Thomas <thomasdigiusto@me.com>
2019-03-01 21:50:46 +0000

    Writes in the file

--- main.c
+++ main.c
@@ -21,21 +21,45 @@ int SAY(char *phrase){
   return 0;
 }
 
-int main(int *argc, char **argv){
+
+static void SAVE_TO_FILE(char *filename, unsigned char *buf, int xsize,
+                                                  int ysize, int wrap){
   FILE *f;
   int i;
+  f = fopen(filename, "w");// Opening the file
+  fprintf(f, "P5\n%d %d\n%d\n", xsize, ysize, 255);
+  for (i = 0; i < ysize; i++)
+    fwrite(buf + i * wrap, 1, xsize, f);
+  fclose(f);
+}
+
+static void READ_THE_FILE(FILE *f){
+  while (!feof(f)){
+    f = f + 1;
+  }
+}
+
+
+int main(int *argc, char **argv){
+  FILE *f;
+
   char message[1024];
   const char *filename, *outfilename;
 
   filename = argv[1];
   outfilename = argv[2];
-  f = fopen(filename,"w");
-
   sprintf(message, "Transforming %s into %s.", filename, outfilename);
   SAY(message);
 
 
-  fclose(f);
+
+  char *buf;
+  buf = "This is a message to write in the file";
+  int xsize = 10;
+  int ysize = 20;
+  int wrap = 2;
+  SAVE_TO_FILE(outfilename, buf, xsize, ysize, wrap);
+
 
 
 
